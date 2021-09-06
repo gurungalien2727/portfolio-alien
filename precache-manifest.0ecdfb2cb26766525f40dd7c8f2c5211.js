b79d3331a78114d7d2338283b82ff8061737677a
Alien Gurung <gurungalien2727@gmail.com>
2021-09-06 02:35:31 -0500

    Updates

--- precache-manifest.0ecdfb2cb26766525f40dd7c8f2c5211.js
+++ precache-manifest.0ecdfb2cb26766525f40dd7c8f2c5211.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e84e86dc027c5ef95b291f706b646a0",
+    "revision": "da33aa9a918a99beb22a45cc0638e487",
     "url": "/portfolio-alien/index.html"
   },
   {
-    "revision": "8bee08d46236c93be239",
+    "revision": "af6d88aa016c09abbf49",
     "url": "/portfolio-alien/static/css/main.4499fbd1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-alien/static/js/2.b619dd07.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8bee08d46236c93be239",
-    "url": "/portfolio-alien/static/js/main.23c5a8ed.chunk.js"
+    "revision": "af6d88aa016c09abbf49",
+    "url": "/portfolio-alien/static/js/main.69da74df.chunk.js"
   },
   {
     "revision": "3682e49949682071c535",
